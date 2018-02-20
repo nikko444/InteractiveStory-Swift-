8e8794823445abbd2b7ede028015f3d34e00ac05
Nick Fialkowski <nikko444@gmail.com>
2018-02-19 19:51:30 -0500

    Initial Commit

---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by nikko444 on 2018-02-19.
+//  Copyright © 2018 nikko444. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
