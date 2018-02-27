4c9398afa77e94e0e687cd7b2673d3110e3628f9
Nick Fialkowski <nikko444@gmail.com>
2018-02-26 21:34:39 -0500

    Moving along the curve

--- InteractiveStory/PageController.swift
+++ InteractiveStory/PageController.swift
@@ -15,7 +15,7 @@ class PageController: UIViewController {
     let artworkView = UIImageView()
     let storyLabel = UILabel()
     let firstChoiceButton = UIButton(type: .system)
-    let secontChoiceButton = UIButton(type: .system)
+    let secondChoiceButton = UIButton(type: .system)
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -28,8 +28,33 @@ class PageController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
         if let page = page {
             artworkView.image = page.story.artwork
+            
+            let attributedString = NSMutableAttributedString(string: page.story.text)
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 10
+            
+            attributedString.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+            
+            storyLabel.attributedText = attributedString
+            
+            if let firstChoice = page.firstChoice {
+                firstChoiceButton.setTitle(firstChoice.title, for: .normal)
+                firstChoiceButton.addTarget(self, action: #selector(PageController.loadFirstChoice), for: .touchUpInside)
+            } else {
+                firstChoiceButton.setTitle("Play Again", for: .normal)
+                firstChoiceButton.addTarget(self, action: #selector(PageController.playAgain), for: .touchUpInside)
+            }
+            
+            if let secondChoice = page.secondChoice {
+                secondChoiceButton.setTitle(secondChoice.title, for: .normal)
+                secondChoiceButton.addTarget(self, action: #selector(PageController.loadSecondChoice), for: .touchUpInside)
+            }
         }
 
     }
@@ -43,13 +68,57 @@ class PageController: UIViewController {
         super .viewWillLayoutSubviews()
         view.addSubview(artworkView)
         artworkView.translatesAutoresizingMaskIntoConstraints = false
-        // artworkView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true ONE WAY DOING THE SAME THING AS BELOW
+        // artworkView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true // ONE WAY OF DOING THE SAME THING AS BELOW
         NSLayoutConstraint.activate([
             artworkView.topAnchor.constraint(equalTo: view.topAnchor),
             artworkView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             artworkView.leftAnchor.constraint(equalTo: view.leftAnchor),
             artworkView.rightAnchor.constraint(equalTo: view.rightAnchor)
             ])
+        
+        view.addSubview(storyLabel)
+        storyLabel.translatesAutoresizingMaskIntoConstraints = false
+        storyLabel.numberOfLines = 0
+        NSLayoutConstraint.activate([
+            storyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            storyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+            storyLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -48.0)
+            ])
+        view.addSubview(firstChoiceButton)
+        firstChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            firstChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80.0)
+            ])
+        
+        view.addSubview(secondChoiceButton)
+        secondChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            secondChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32)
+            ])
+    }
+    
+    @objc func loadFirstChoice () {
+        if let page = page, let firstChoice = page.firstChoice {
+            let nextPage = firstChoice.page
+            let pageController = PageController(page: nextPage)
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    @objc func loadSecondChoice () {
+        if let page = page, let secondChoice = page.secondChoice {
+            let nextPage = secondChoice.page
+            let pageController = PageController(page: nextPage)
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    @objc func playAgain () {
+        navigationController?.popToRootViewController(animated: true)
     }
 
 }
